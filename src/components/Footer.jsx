540f4c7133c0d7020989ac980d03fb9e6c6e0c32
Christian Monzon <christian.monzon@newsela.com>
2023-11-17 15:17:51 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,14 +40,7 @@ function Footer() {
              aria-label="Visit Open Sauced GitHub Organisation">
             <FaDev className="text-offWhite hover:text-accent" />
           </a>
-        </li>
-        <li className="p-2 sm:mr-5">
-          <a href="https://www.dev.to/opensauced"
-             target="_blank" rel="noreferrer"
-             aria-label="Visit Open Sauced GitHub Organisation">
-            <FaDev className="text-offWhite hover:text-accent" />
-          </a>
-        </li>        
+        </li>      
       </ul>
     </Container>
   );
